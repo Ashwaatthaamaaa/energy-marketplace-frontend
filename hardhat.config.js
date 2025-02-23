9aa2d599800cfbba0bc82dedc1b3017fdd1d2728
Ashwaatthaamaaa <sarthakk145@gmail.com>
2025-02-23 14:34:18 +0530

    Configure Hardhat network settings for Sepolia testnet deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+require("dotenv").config();
+
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 11155111, // Sepolia chain ID
+    },
+  },
 };
